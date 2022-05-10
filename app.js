7d014df76a8374acea2cc0ed067fdb3beea5782a
abirhasn07 <iamabirhasan07@gmail.com>
2022-05-11 00:29:13 +0600

    day1 complete

---
+++ app.js
@@ -0,0 +1,42 @@
+TweenMax.from('.navbar',1,{
+    delay:.3,
+    x:-40,
+    opacity:0,
+    ease:Expo.easeInOut
+    })
+TweenMax.from('.header-headline',2,{
+    delay:.5,
+    y:80,
+    opacity:0,
+    ease:Expo.easeInOut
+    })
+TweenMax.from('.header-subtitle',3,{
+    delay:.5,
+    y:20,
+    opacity:0,
+    ease:Expo.easeInOut
+    })
+TweenMax.from('.cta',4,{
+    delay:.5,
+    y:20,
+    opacity:0,
+    ease:Expo.easeInOut
+    })
+TweenMax.from('form',5,{
+    delay:0.3,
+    y:80,
+    opacity:0,
+    ease:Expo.easeInOut
+    })
+TweenMax.from('.product-info',6,{
+    delay:0.5,
+    x:-100,
+    opacity:0,
+    ease:Expo.easeInOut
+    })
+TweenMax.from('.product-card',7,{
+    delay:0.5,
+    y:200,
+    opacity:0,
+    ease:Expo.easeInOut
+    })
