91ed8679cd16da45ae230ae920185271aaf6705b
abirhasn07 <iamabirhasan07@gmail.com>
2022-05-11 23:52:39 +0600

    responsive section done

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+const line1=document.querySelector('.line1');
+const line2=document.querySelector('.line2');
+const line3=document.querySelector('.line3');
+const sidebar=document.querySelector('.nav-list');
+
+
+const showMenu=()=>{
+    line1.classList.toggle('active');
+    line2.classList.toggle('active');
+    line3.classList.toggle('active');
+    sidebar.classList.toggle("active")
+
+}
+
+
 TweenMax.from('.navbar',1,{
     delay:.3,
     x:-40,
